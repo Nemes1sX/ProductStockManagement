7c59e343af063b5ee235cc74b1e7a5b52f16ecf1
Nemes1sX <kazstonk@gmail.com>
2024-09-01 01:22:50 +0300

    Separate of concerns

---
+++ app/Services/ImportProductService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class ImportProductService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
