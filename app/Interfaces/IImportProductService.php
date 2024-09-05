759b6e75c38fbff1bb3127519d8d550a104d1be1
Nemes1sX <kazstonk@gmail.com>
2024-09-06 00:00:37 +0300

    Refactor import products

--- app/Interfaces/IImportProductService.php
+++ app/Interfaces/IImportProductService.php
@@ -4,5 +4,5 @@ namespace App\Interfaces;
 
 interface IImportProductService
 {
-    function importProducts(array $importProducts) : array;
+    function importProducts(array $importProducts) : void;
 }
--- app/Services/ImportProductService.php
+++ app/Services/ImportProductService.php
@@ -18,49 +18,24 @@ class ImportProductService implements IImportProductService
         //
     }
 
-    public function importProducts(array $importProducts) : array
+    public function importProducts(array $importProducts) : void
     {
-        $data = [];
-        $tags = [];
-        foreach ($importProducts as $product) {
-            $existingProduct = Product::where('sku', $product->sku)->first();
-            if (!$existingProduct) {
-                $data[] = [
-                    'sku' => $product->sku,
-                    'size' => $product->size,
-                    'description' => $product->description,
-                    'photo' => $product->photo,
-                    'updated_at' => Carbon::createFromFormat('Y-m-d', $product->updated_at),
-                ];
-                $tags[] = $product->tags;
+        if (count($importProducts) == 0) {
+            return;
+        }    
+        foreach ($importProducts as $importProduct) {
+            $product = Product::firstOrCreate(
+                ['sku' => $importProduct->sku],
+                [
+                 'description' => $importProduct->description,
+                 'size' => $importProduct->size,
+                 'photo' => $importProduct->photo,    
+                ]
+                );
+            foreach ($importProduct->tags as $productTag) {
+                $tag = Tag::firstOrCreate(['name' => $productTag->title], []);
+                $product->tags()->syncWithoutDetaching($tag->id);
             }
         }
-
-        if (count($data) >= 1) {
-            foreach (array_chunk($data, 1000) as $chunk) {
-                Product::insert($chunk);
-            }
-            if (count($tags) >= 1) {
-                for ($i = 0; $i < count($data); $i++) {
-                    $existingProduct = Product::where('sku', $data[$i]['sku'])->first();
-                    if (!$existingProduct) {
-                        continue;
-                    }
-                    for ($j = 0; $j < count($tags); $j++) {
-                        for ($k = 0; $k < count($tags[$j]); $k++) {
-                            $existingTag = Tag::where('name', $tags[$j][$k]->title)->first();
-                            if (!$existingTag) {
-                                $newTag = Tag::create(['name' => $tags[$j][$k]->title]);
-                                $existingProduct->tags()->syncWithoutDetaching($newTag->id);
-                            } else {
-                                $existingProduct->tags()->syncWithoutDetaching($existingTag->id);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return $data;
     }
 }
